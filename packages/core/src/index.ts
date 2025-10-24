c90c227b04d2617be37280119ed67b3b8cbbbd8b
Adam Dean <adam@crypto2099.io>
2025-10-24 10:46:39 -0700

    Fix the index.ts to explicitly export all functions so that we get better downstream type-hinting

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
-export * from './mesh/get-admin';
-export * from './mesh/native-script';
-export * from './mesh/wrangler'
-export * from './hydra/utxo';
-export * from './tx3/submit-tx';
-export * from './utils/verify-signature'
-export * from './utils/chunk-string'
+export { getAdmin } from './mesh/get-admin';
+export { createMultisigAddress } from './mesh/native-script';
+export { Wrangler } from './mesh/wrangler';
+export { getUtxoSet, queryUtxoByAddress } from './hydra/utxo';
+export { submitTx } from './tx3/submit-tx';
+export { bufferToHex, bufferToAscii, verifySignature } from './utils/verify-signature';
+export { chunkString } from './utils/chunk-string';
